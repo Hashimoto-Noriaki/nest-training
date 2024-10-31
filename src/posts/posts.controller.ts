7ba2f7270cf06c64e8a60aab5149a9c54fc09131
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-11-01 01:59:27 +0900

    サービスを作成

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,9 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { PostsService } from './posts.service';
 
 @Controller('posts')
 export class PostsController {
+    //constructorは、PostsControllerクラスのインスタンスが作成されるときに自動的に呼ばれる特殊なメソッド
+    //PostsController内でPostsServiceを利用するために、依存性注入によってインスタンスを自動的に提供する仕組み 
+    //private readonlyで定義することにより、外部からのアクセスや再代入が制限
+    constructor(private readonly postsService: PostsService){}
+
   @Get()
   findAll() {
-    return '投稿を全て取得';
+    return this.postsService.findAll();
   }
 }
